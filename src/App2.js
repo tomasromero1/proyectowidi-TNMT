62cfcb0233ae96b6761a9cd956dccd5d27ba5519
thiagocorsino1 <corsinothiago1@gmail.com>
2024-07-31 15:03:49 -0300

    componente footer

--- src/App2.js
+++ src/App2.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { Footer } from "./components/footer/footer";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>App ESI</h1>
-        <h2>inicio</h2>
+        <h2>inicio 2</h2>
         <p>
-          Edit <code>src/App.js 2</code> and save to reload.
+          Edit <code>src/App.jssss</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="index.js"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           pantalla 2
         </a>
       </header>
-      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useRef, useEffect } from "react";
+import './footer.css'
+
+import homeIcon from '../../logo.svg';  
+import infoIcon from '../../logo.svg';  
+import playIcon from '../../logo.svg';
+
+export function Footer () {
+    return(
+        <footer className="footer">
+        <a href="" className="footer-button">
+          <img src={homeIcon} alt="Inicio" className="button-icon"/>
+          Inicio
+        </a>
+        <a href="" className="footer-button">
+          <img src={infoIcon} alt="Información" className="button-icon"/>
+          Información
+        </a>
+        <a href="" className="footer-button">
+          <img src={playIcon} alt="Jugar" className="button-icon"/>
+          Jugar
+        </a>
+      </footer>
+    )
+}
+
+
