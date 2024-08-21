251dc3212215e69487179c55515a5fbfeb17340e
lopeznehuen <nehuenl19@gmail.com>
2024-08-20 21:17:29 -0300

    Agregando Nombres

--- src/home.js
+++ src/home.js
@@ -5,6 +5,11 @@ const Home = () => {
     <div>
       <h2>Inicio</h2>
       <p>Pagina de inicio</p>
+      <div>Esta Pagina Esta creada por:
+        <p>Nehuen Lopez</p>
+        <p>Tomas Romero</p>
+        <p>Thiago Corsino</p>
+      </div>
     </div>
   );
 }
