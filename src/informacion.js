9b969bbd0aa05fba70d4b6b644b9ae534df18679
lopeznehuen <nehuenl19@gmail.com>
2024-08-20 21:13:00 -0300

    Agregando Informacion

--- src/informacion.js
+++ src/informacion.js
@@ -6,6 +6,9 @@ const info = () => {
       <h2>INFO</h2>
       <h1>Informacion de ESI</h1>
       <p>La Educación Sexual Integral (ESI) es una política pública reconocida por ley que busca enseñar habilidades y conocimientos sobre la sexualidad, permitiendo a los jóvenes tomar decisiones informadas y autónomas</p>
+        <p>Derechos sexuales y reproductivos: Este eje se centra en la promoción y defensa de los derechos sexuales y reproductivos como parte integral de los derechos humanos. Incluye temáticas como el acceso a la información y a servicios de salud sexual y reproductiva, la prevención del embarazo no deseado, el derecho al aborto legal y seguro, y el consentimiento informado.</p>
+        <p>Género y diversidad: Este eje aborda la importancia de reconocer y respetar la diversidad de género y orientación sexual, promoviendo la equidad y la inclusión. Se trabaja en la deconstrucción de estereotipos y roles de género, y se busca fomentar la igualdad de oportunidades y el respeto hacia todas las personas, independientemente de su identidad de género u orientación sexual.</p>
+        <p>Prevención de la violencia y el abuso: La ESI trabaja en la prevención y la detección temprana de situaciones de violencia y abuso en las relaciones interpersonales, familiares y sociales. Este eje incluye el análisis de las distintas formas de violencia, como la violencia de género, el acoso escolar y el ciberacoso, y propone estrategias de prevención, denuncia y acompañamiento a las víctimas.</p>
     </div>
   );
 }
