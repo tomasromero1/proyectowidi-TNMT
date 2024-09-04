6e5c848144014b5863ddf287ab5fbfedf092a047
thiagocorsino1 <corsinothiago1@gmail.com>
2024-09-04 12:55:20 -0300

    modificando propiedad Router

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import './App.css';
 import { Header } from "./modules/components/header/header";
 import { Footer } from "./modules/components/footer/footer";
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Info from './informacion';
+import Play from './play';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <div className="content-container">
-        {}
-        <Footer/>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="content-container">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/informacion" element={<Info />} />
+            <Route path="/play" element={<Play />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/modules/components/footer/footer.jsx
+++ src/modules/components/footer/footer.jsx
@@ -1,39 +1,30 @@
-import './footer.css'
-
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Home from '../../../home';
-import Info from '../../../informacion';
-import Play from '../../../play';
-
+import './footer.css';
+import { Link } from 'react-router-dom';
 import homeIcon from '../img/icon_home.png';  
 import infoIcon from '../img/book_icon.png';  
 import playIcon from '../img/joystick_icon.png';
 
-
-export function Footer () {
-    return(
-        <Router>
-      <div>
+export function Footer() {
+    return (
         <footer className="footer">
-        <div className="footer-button">
-          <Link to="/home"><img src={homeIcon} alt="Inicio" className="button-icon"/>
-          Inicio</Link>
-        </div>
-        <div className="footer-button">
-          <Link to="/informacion"><img src={infoIcon} alt="Información" className="button-icon"/>
-          Información</Link>
-        </div>
-        <div className="footer-button">
-          <Link to="/play"><img src={playIcon} alt="Jugar" className="button-icon"/>
-          Jugar</Link>
-        </div>
-      </footer>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/informacion" element={<Info />} />
-          <Route path="/play" element={<Play />} />
-        </Routes>
-      </div>
-    </Router>
-    )
+            <div className="footer-button">
+                <Link to="/home">
+                    <img src={homeIcon} alt="Inicio" className="button-icon" />
+                    Inicio
+                </Link>
+            </div>
+            <div className="footer-button">
+                <Link to="/informacion">
+                    <img src={infoIcon} alt="Información" className="button-icon" />
+                    Información
+                </Link>
+            </div>
+            <div className="footer-button">
+                <Link to="/play">
+                    <img src={playIcon} alt="Jugar" className="button-icon" />
+                    Jugar
+                </Link>
+            </div>
+        </footer>
+    );
 }
