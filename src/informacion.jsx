3823caace297a8dd7e883086b37a103e436aaa29
tomasromero1 <tomirc06@gmail.com>
2024-10-14 13:21:54 -0300

    Agregando tabla

--- src/informacion.jsx
+++ src/informacion.jsx
@@ -18,4 +18,37 @@ const info = () => {
   );
 }
 
+    <table className="esi-table">
+      <thead>
+        <tr>
+          <th>Aspecto</th>
+          <th>Descripción</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Consentimiento</td>
+          <td>Es fundamental que todas las interacciones sexuales sean consentidas y libres.</td>
+        </tr>
+        <tr>
+          <td>Respeto a la diversidad</td>
+          <td>Aceptar y valorar todas las identidades sexuales y de género.</td>
+        </tr>
+          {/* Agrega más filas aquí con otros aspectos importantes de la ESI */}
+        <tr>
+          <td>Salud sexual y reproductiva</td>
+          <td>Promover el cuidado del cuerpo y la prevención de enfermedades de transmisión sexual.</td>
+        </tr>
+        <tr>
+          <td>Equidad de género</td>
+          <td>Trabajar por la igualdad de oportunidades y derechos entre hombres y mujeres.</td>
+        </tr>
+        <tr>
+          <td>Afectividad</td>
+          <td>Fomentar relaciones basadas en el respeto, la confianza y la comunicación.</td>
+        </tr>
+      </tbody>
+    </table>
+  ;
+
 export default info;
