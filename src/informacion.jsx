ec3b020468b70f1e2da09122e12a7e9d1ee1e25c
tomasromero1 <tomirc06@gmail.com>
2024-10-14 13:17:05 -0300

    Agregando informacion (consejos

--- src/informacion.jsx
+++ src/informacion.jsx
@@ -8,6 +8,12 @@ const info = () => {
         <p>Derechos sexuales y reproductivos: Este eje se centra en la promoción y defensa de los derechos sexuales y reproductivos como parte integral de los derechos humanos. Incluye temáticas como el acceso a la información y a servicios de salud sexual y reproductiva, la prevención del embarazo no deseado, el derecho al aborto legal y seguro, y el consentimiento informado.</p>
         <p>Género y diversidad: Este eje aborda la importancia de reconocer y respetar la diversidad de género y orientación sexual, promoviendo la equidad y la inclusión. Se trabaja en la deconstrucción de estereotipos y roles de género, y se busca fomentar la igualdad de oportunidades y el respeto hacia todas las personas, independientemente de su identidad de género u orientación sexual.</p>
         <p>Prevención de la violencia y el abuso: La ESI trabaja en la prevención y la detección temprana de situaciones de violencia y abuso en las relaciones interpersonales, familiares y sociales. Este eje incluye el análisis de las distintas formas de violencia, como la violencia de género, el acoso escolar y el ciberacoso, y propone estrategias de prevención, denuncia y acompañamiento a las víctimas.</p>
+      <h3>Consejos.</h3>
+        <p>Infórmate: Busca información confiable sobre sexualidad en libros, internet o hablando con personas de confianza.</p>
+        <p>Cuida tu cuerpo: Conoce tu cuerpo y aprende a cuidarlo.</p>
+        <p>Respeta a los demás: Respeta los límites de los demás y exige que te respeten.</p>
+        <p>Toma decisiones informadas: Antes de tomar decisiones sobre tu sexualidad, infórmate y reflexiona sobre las consecuencias.</p>
+        <p>Busca ayuda si la necesitas: Si tienes alguna duda o preocupación, no dudes en hablar con un adulto de confianza, un profesor o un profesional de la salud.</p>
     </div>
   );
 }
