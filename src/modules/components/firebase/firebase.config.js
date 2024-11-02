11c03e2ef42d7b9393bc408c47f66913560e97c0
thiagocorsino1 <corsinothiago1@gmail.com>
2024-11-02 17:30:38 -0300

    modificando crud login

--- src/modules/components/firebase/firebase.config.js
+++ src/modules/components/firebase/firebase.config.js
@@ -1,13 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyB97MvSqYFu8j_79OO6RiX84rvquRWywXs",
+  authDomain: "proyecto-tnmt.firebaseapp.com",
+  projectId: "proyecto-tnmt",
+  storageBucket: "proyecto-tnmt.appspot.com",
+  messagingSenderId: "21392943924",
+  appId: "1:21392943924:web:79ce9cb706fe1247779361",
+  measurementId: "G-4TLWLCH7C2"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app); // Agregando autenticación
-const provider = new GoogleAuthProvider(); // Configuración para Google
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 export { db, auth, provider };
