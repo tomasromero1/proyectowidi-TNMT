d5a71858c98fd211527971753393e12e61311e60
tomasromero1 <tomirc06@gmail.com>
2024-11-05 22:11:01 -0300

    Constante firebaseConfig

--- src/modules/components/firebase/firebase.jsx
+++ src/modules/components/firebase/firebase.jsx
@@ -2,3 +2,7 @@ import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+const firebaseConfig = {
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
