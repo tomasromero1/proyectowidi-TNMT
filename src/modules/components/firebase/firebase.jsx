1b652902235875eb5b13f2b7dbfcc3ffe591504b
tomasromero1 <tomirc06@gmail.com>
2024-11-05 22:16:23 -0300

    Agregando cons y return

--- src/modules/components/firebase/firebase.jsx
+++ src/modules/components/firebase/firebase.jsx
@@ -6,3 +6,30 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+
+function Preguntas() {
+    const [preguntas, setPreguntas] = useState([]);
+
+    useEffect(() => {
+    const unsubscribe = db.collection('preguntas').onSnapshot((snapshot) => {
+        const preguntasData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+        }));
+        setPreguntas(preguntasData);
+    });
+    return (
+        <div>
+          <h2>Preguntas</h2>
+          <ul>
+            {preguntas.map((pregunta) => (
+              <li key={pregunta.id}>
+                <strong>{pregunta.pregunta}</strong>
+                <p>Por {pregunta.usuario}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+}
