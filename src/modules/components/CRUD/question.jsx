20225c56eaa640ab6c646b1232c9280fe1b830e8
tomasromero1 <tomirc06@gmail.com>
2024-11-05 21:36:44 -0300

    Agregango useEffect

--- src/modules/components/CRUD/question.jsx
+++ src/modules/components/CRUD/question.jsx
@@ -2,4 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 function PreguntasRespuestas() {
 const [preguntas, setPreguntas] = useState([]);
+
+useEffect(() => {
+    // Aquí realizarías la petición al backend para obtener las preguntas
+    fetch('/api/preguntas')
+    .then(response => response.json())
+    .then(data => setPreguntas(data))
+    .catch(error => console.error(error));
+}, []);
+
 }
