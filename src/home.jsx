5c20a8a019281b4e5df9f65a39558faae68e063a
thiagocorsino1 <corsinothiago1@gmail.com>
2024-11-12 10:40:00 -0300

    Modificando componente home

--- src/home.jsx
+++ src/home.jsx
@@ -6,10 +6,10 @@ const Home = () => {
   return (
     <div>
       <h2 className='title'>Inicio</h2>
-      <div className='home'>
-      <p>La Educación Sexual Integral (ESI) es una política pública reconocida por ley que busca enseñar habilidades y conocimientos sobre la sexualidad, permitiendo a los jóvenes tomar decisiones informadas y autónomas</p>
+      <div className='homee'>
+        <p className='tex'>La Educación Sexual Integral (ESI) es una política pública reconocida por ley que busca enseñar habilidades y conocimientos sobre la sexualidad, permitiendo a los jóvenes tomar decisiones informadas y autónomas</p>
       </div>
-      <button className='info'>
+      <button>
           <Link to="/Informacion">
               <p>Más informacion sobre ESI</p>
            </Link>
